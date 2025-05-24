1a4f6098015b7f4dbc20d7e096991d07572510f7
Youngrae Cho <yrc9229@gmail.com>
2025-05-24 12:23:59 +0900

    템플릿 batch

---
+++ spring-boot-kotlin-template/batch/src/test/kotlin/com/example/template/BatchApplicationTest.kt
@@ -0,0 +1,11 @@
+package com.example.template
+
+import com.example.template.batch.test.ContextTest
+import org.junit.jupiter.api.Test
+
+class BatchApplicationTest: ContextTest() {
+
+    @Test
+    fun contextLoads() {
+    }
+}
