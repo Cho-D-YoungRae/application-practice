4c445d8e5f123faf2b158b93b36494687f725637
Youngrae Cho <yrc9229@gmail.com>
2025-07-19 21:01:37 +0900

    like: kafka topic 초기화

---
+++ projects/250528-like/core/core-event/src/main/kotlin/com/example/like/core/event/local/PostLikeInitializer.kt
@@ -0,0 +1,35 @@
+package com.example.like.core.event.local
+
+import com.example.like.core.event.PostLikeEventProperties
+import org.apache.kafka.clients.admin.NewTopic
+import org.slf4j.LoggerFactory
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.kafka.core.KafkaAdmin
+import org.springframework.stereotype.Component
+
+@Component
+@ConditionalOnProperty(
+    name = ["app.event.post-like.init.enabled"],
+    havingValue = "true",
+    matchIfMissing = false,
+)
+class PostLikeInitializer(
+    private val kafkaAdmin: KafkaAdmin,
+    private val properties: PostLikeEventProperties,
+) : ApplicationRunner {
+    
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    override fun run(args: ApplicationArguments?) {
+        log.info("{} 실행. topicName={}", this.javaClass.simpleName, properties.topicName)
+        kafkaAdmin.createOrModifyTopics(
+            NewTopic(
+                properties.topicName,
+                3,
+                2
+            )
+        )
+    }
+}
